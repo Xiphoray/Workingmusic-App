f3df5a3bbe433bffec90cd1f7258ade8fbd72c7d
Xiphoray <351014842@qq.com>
2017-07-25 17:29:32 +0800

    ui初步完成

--- app/src/main/java/text/workingmusic/MainActivity.java
+++ app/src/main/java/text/workingmusic/MainActivity.java
@@ -1,9 +1,8 @@
 package text.workingmusic;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,9 +12,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    final int locationb1[] = new int[2];
+    final int locationb2[] = new int[2];
+
+    boolean playon = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +34,17 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -43,7 +54,40 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        final View button1 = findViewById(R.id.button);
+        final View button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                playon = false;
+                button1.getLocationOnScreen(locationb1);
+                button2.getLocationOnScreen(locationb2);
+                ObjectAnimator.ofFloat(button1, "translationY", 0 , locationb2[1]-locationb1[1]).setDuration(500).start();
+                View view;
+                view=findViewById(R.id.workingmusicnow);
+                setHideAnimation(view,450);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                playon = true;
+                button1.getLocationOnScreen(locationb1);
+                button2.getLocationOnScreen(locationb2);
+                ObjectAnimator.ofFloat(button2, "translationY", 0 , locationb1[1]-locationb2[1]).setDuration(500).start();
+                View view;
+                view=findViewById(R.id.workingmusicnow);
+                setShowAnimation(view,450);
+            }
+        });
+
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -70,9 +114,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -82,15 +126,135 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        View view;
         if (id == R.id.nav_workingmusic) {
+            if(playon == true)
+            {
+                view=findViewById(R.id.workingmusicnow);
+                view.setVisibility(View.VISIBLE);
+            }
+            else
+            {
+                view=findViewById(R.id.workingmusicnow);
+                view.setVisibility(View.INVISIBLE);
+            }
+            view=findViewById(R.id.workingmusicstop);
+            view.setVisibility(View.VISIBLE);
+            view=findViewById(R.id.informationnow);
+            view.setVisibility(View.INVISIBLE);
 
         } else if (id == R.id.nav_information) {
+            view=findViewById(R.id.workingmusicnow);
+            view.setVisibility(View.INVISIBLE);
+            view=findViewById(R.id.workingmusicstop);
+            view.setVisibility(View.INVISIBLE);
+            view=findViewById(R.id.informationnow);
+            view.setVisibility(View.VISIBLE);
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private  AlphaAnimation mHideAnimation= null;
+
+    private  AlphaAnimation mShowAnimation= null;
+    /**
+     * View渐现动画效果
+     */
+    public  void setShowAnimation( final View view, int duration)
+    {
+        if (null == view || duration < 0)
+        {
+            return;
+        }
+        if (null != mShowAnimation)
+        {
+            mShowAnimation.cancel();
+        }
+        mShowAnimation = new AlphaAnimation(0.0f, 1.0f);
+        mShowAnimation.setDuration(duration);
+        mShowAnimation.setFillAfter(false);
+        mShowAnimation.setAnimationListener(new AnimationListener()
+        {
+
+            @Override
+            public void onAnimationStart(Animation arg0)
+            {
+                view.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0)
+            {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0)
+            {
+
+
+                View button1=findViewById(R.id.button);
+                View button2=findViewById(R.id.button2);
+//                button1.getLocationOnScreen(locationb1);
+//                button2.getLocationOnScreen(locationb2);
+                ObjectAnimator.ofFloat(button2, "translationY", 0 , 0).setDuration(500).start();
+                button2.setVisibility(View.INVISIBLE);
+                button1.setVisibility(View.VISIBLE);
+            }
+        });
+        view.startAnimation(mShowAnimation);
+    }
+
+    /**
+     * View渐隐动画效果
+     */
+    public  void setHideAnimation( final View view, int duration)
+    {
+        if (null == view || duration < 0)
+        {
+            return;
+        }
+
+        if (null != mHideAnimation)
+        {
+            mHideAnimation.cancel();
+        }
+        // 监听动画结束的操作
+        mHideAnimation = new AlphaAnimation(1.0f, 0.0f);
+        mHideAnimation.setDuration(duration);
+        mHideAnimation.setFillAfter(true);
+        mHideAnimation.setAnimationListener(new AnimationListener()
+        {
+
+            @Override
+            public void onAnimationStart(Animation arg0)
+            {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0)
+            {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0)
+            {
+                view.setVisibility(View.INVISIBLE);
+                View button1=findViewById(R.id.button);
+                View button2=findViewById(R.id.button2);
+//                button1.getLocationOnScreen(locationb1);
+//                button2.getLocationOnScreen(locationb2);
+                ObjectAnimator.ofFloat(button1, "translationY", 0 , 0).setDuration(500).start();
+                button1.setVisibility(View.INVISIBLE);
+                button2.setVisibility(View.VISIBLE);
+            }
+        });
+        view.startAnimation(mHideAnimation);
     }
 }
